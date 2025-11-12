b2a5147a000e147b28dac3b6c3b85143af92ce26
F88 <685250+F88@users.noreply.github.com>
2025-11-12 12:29:31 +0900

    Update manifest to clarify maskable icon entries and correct icon sources

--- app/manifest.ts
+++ app/manifest.ts
@@ -57,12 +57,13 @@ export default function manifest(): MetadataRoute.Manifest {
         type: 'image/png',
         purpose: 'any',
       },
-      // {
-      // src: '/icons/icon-192x192-maskable.png',
-      // sizes: '192x192',
-      // type: 'image/png',
-      // purpose: 'maskable',
-      // },
+      {
+        // src: '/icons/icon-192x192-maskable.png',
+        src: '/icons/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
       {
         src: '/icons/icon-256x256.png',
         sizes: '256x256',
@@ -81,12 +82,13 @@ export default function manifest(): MetadataRoute.Manifest {
         type: 'image/png',
         purpose: 'any',
       },
-      // {
-      // src: '/icons/icon-512x512-maskable.png',
-      // sizes: '512x512',
-      // type: 'image/png',
-      // purpose: 'maskable',
-      // },
+      {
+        // src: '/icons/icon-512x512-maskable.png',
+        src: '/icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
     ],
     // Add screenshots to enable Richer PWA Install UI on desktop and mobile
     // Ref: https://developer.chrome.com/docs/web-platform/web-app-install-banners/#add-screenshots
