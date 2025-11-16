5d6d0caca488942d3d7bc61b28a6e61a21ddb581
F88 <685250+F88@users.noreply.github.com>
2025-11-16 23:03:07 +0900

    feat: define PlayMode and associated state types for improved playlist handling

--- types/mugen-protopedia.types.ts
+++ types/mugen-protopedia.types.ts
@@ -5,6 +5,19 @@
  */
 export type PlayMode = 'normal' | 'playlist';
 
+type BasePlayModeState<T extends PlayMode> = {
+  playmode: T;
+};
+
+export type NormalPlayModeState = BasePlayModeState<'normal'>;
+
+export type PlaylistPlayModeState = BasePlayModeState<'playlist'> & {
+  ids: number[];
+  title?: string;
+};
+
+export type PlayModeState = NormalPlayModeState | PlaylistPlayModeState;
+
 /**
  * Control panel mode of the ProtoPedia Viewer.
  * - 'normal': Standard control panel mode.
