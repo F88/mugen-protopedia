d826ca5628e188d5f2da08ad490ecda353503139
F88 <685250+F88@users.noreply.github.com>
2025-11-17 21:02:48 +0900

    fix: set Image priority to false in PrototypeMainImage component

--- components/prototype/prototype-main-image.tsx
+++ components/prototype/prototype-main-image.tsx
@@ -33,7 +33,7 @@ export const PrototypeMainImage = ({
   return (
     <>
       <Image
-        priority={true}
+        priority={false}
         src={imageUrl}
         alt={altText}
         width={600}
