97a80599654aefe8198d28a0157bbeebf8de9900
F88 <685250+F88@users.noreply.github.com>
2025-12-01 09:18:26 +0900

    refactor: remove unused StarAlignmentSection import from HelloWorldPage

--- app/observatory/hello-world/page.tsx
+++ app/observatory/hello-world/page.tsx
@@ -33,7 +33,6 @@ import {
 import { MaternityHospitalSection } from './components/maternity-hospital-section';
 import { NewbornsSection } from './components/newborns-section';
 import { PowerOfDeadlinesSection } from './components/power-of-deadlines-section';
-import { StarAlignmentSection } from './components/star-alignment-section';
 import { WeekendWarriorSection } from './components/weekend-warrior-section';
 import { StarAlignmentSection2 } from './components/star-alignment-section-2';
 
